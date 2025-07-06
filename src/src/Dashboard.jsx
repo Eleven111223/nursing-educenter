40d1aed4d2dac5d67c30a391291b390f1b574601
Eleven111223 <ichonsangguniangkabataan@gmail.com>
2025-07-06 23:45:25 +0800

    Create Dashboard.jsx

---
+++ src/src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to Nursing EduCenter!</h1>
+      <p>This is your dashboard after login.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
